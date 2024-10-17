c7ccd3b270a5d4bd3125e0dc3d2728ca5d84f99b
Bohdan <bohdan5621@gmail.com>
2024-10-17 18:46:53 +0300

    Build Recipe and Tag API

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,18 @@
 """
 Tests for models.
 """
+
+from decimal import Decimal
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
+
+
+def create_user(email='email@example.com', password='password123'):
+    """Create and return new user"""
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
@@ -46,3 +56,37 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful"""
+        user = get_user_model().objects.create_user(
+            'example@gmail.com',
+            'password123',
+        )
+
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Simple recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Simple recipe description',
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
+        
+    def test_create_ingredient(self):
+        """Test creagin an ingredient is successful"""
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user=user,
+            name='Ingredient1'
+        )
+        
+        self.assertEqual(str(ingredient), ingredient.name)
